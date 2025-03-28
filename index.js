d87576268e139f5dfc6bf497b5423a9e62503ee7
OldPole <ipetr6828@gmail.com>
2025-03-29 00:12:41 +0300

    added render of questions and events. Fixed markup

--- index.js
+++ index.js
@@ -15,21 +15,6 @@ const data = [
         ]
     },
     {
-        question: 'Оцените чистоту в ресторане:',
-        type: 'radio',
-        answers: [
-            {
-                value: 'Отлично',
-            },
-            {
-                value: 'Норм',
-            },
-            {
-                value: 'Плохо',
-            },
-        ]
-    },
-    {
         question: 'Оцените дружелюбие наших сотрудников',
         type: 'radio',
         answers: [
@@ -41,18 +26,6 @@ const data = [
             },
             {
                 value: 'Плохо',
-            },
-        ]
-    },
-    {
-        question: 'Ваш заказ был собран правильно?',
-        type: 'radio',
-        answers: [
-            {
-                value: 'Да',
-            },
-            {
-                value: 'Нет',
             },
         ]
     },
@@ -103,14 +76,77 @@ const data = [
                 value: 'Новый бургер с курицей',
             },
             {
-                value: 'Новый бургер с рыбой',
-            },
-            {
-                value: 'Новый бургер со свининной',
-            },
-            {
                 value: 'Новый фирменный напиток',
             },
         ]
     },
 ]
+
+let localResults = {};
+
+const survey = document.getElementById('survey');
+const questions = document.getElementById('questions');
+const contacts = document.getElementById('contacts');
+const btnNext = document.getElementById('btn-next');
+const btnSend = document.getElementById('btn-send');
+const indicator = document.getElementById('indicator');
+
+const renderQuestions = (index) => {
+    questions.dataset.currentstep = index;
+
+    const renderAnswers = () => data[index].answers
+    .map((answer) => `
+        <li>
+            <label>
+                <input class="answer-input" type=${data[index].type} name="question${index}" value=${answer.value}>
+                ${answer.value}
+            </label>
+        </li>
+    `)
+    .join('');
+
+    questions.innerHTML = `
+    <div class="survey-questions-item">
+        <div class="survey-questions-item__question">${data[index].question}</div>
+        <ul class="survey-questions-item__answers">${renderAnswers()}</ul>
+    </div>
+    `;
+}
+
+const renderUniqueQuestion = () => {
+    
+}
+
+const renderContacts = () => {
+
+}
+
+const renderIndicator = () => {
+
+}
+
+survey.addEventListener('change', (event) => {
+    if(event.target.classList.contains('answer-input')) {
+        localResults[event.target.name] = event.target.value;
+        console.log(localResults);
+        btnNext.disabled = false;
+    }
+});
+
+survey.addEventListener('click', (event) => {
+    if(event.target.classList.contains('btn-next')){
+        const nextQuestionIndex = Number(questions.dataset.currentstep) + 1;
+
+        if(nextQuestionIndex === 4){
+            renderUniqueQuestion();
+        } else if (nextQuestionIndex === data.length) {
+            renderContacts();
+        } else {
+            renderQuestions(nextQuestionIndex);
+        }
+
+        btnNext.disabled = true;
+    }
+});
+
+renderQuestions(0);
